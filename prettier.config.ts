e52daf929cf3a1501591b1061069e88fb14b57d3
trev <me@trev.xyz>
2025-11-11 11:21:19 -0500

    style: use double quotes

--- prettier.config.ts
+++ prettier.config.ts
@@ -1,24 +1,22 @@
-import type { Config } from 'prettier';
+import type { Config } from "prettier";
 
 const config: Config = {
 	useTabs: true,
-	singleQuote: true,
-	trailingComma: 'none',
 	printWidth: 100,
 	plugins: [
-		'prettier-plugin-svelte',
-		'prettier-plugin-tailwindcss',
-		'@ianvs/prettier-plugin-sort-imports'
+		"prettier-plugin-svelte",
+		"prettier-plugin-tailwindcss",
+		"@ianvs/prettier-plugin-sort-imports",
 	],
 	overrides: [
 		{
-			files: '*.svelte',
+			files: "*.svelte",
 			options: {
-				parser: 'svelte'
-			}
-		}
+				parser: "svelte",
+			},
+		},
 	],
-	tailwindStylesheet: './src/app.css'
+	tailwindStylesheet: "./src/app.css",
 };
 
 export default config;
