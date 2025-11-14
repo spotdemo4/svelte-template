ed734dcf9fd5e6d6a56dc0d16a45d9018d3b8a7b
trev <me@trev.xyz>
2025-11-13 23:02:52 -0500

    style: use new nixfmt

--- flake.nix
+++ flake.nix
@@ -27,151 +27,156 @@
     };
   };
 
-  outputs = {
-    nixpkgs,
-    utils,
-    trev,
-    semgrep-rules,
-    ...
-  }:
-    utils.lib.eachDefaultSystem (system: let
-      pkgs = import nixpkgs {
-        inherit system;
-        overlays = [
-          trev.overlays.packages
-          trev.overlays.libs
-        ];
-      };
-      node = pkgs.nodejs_24;
-      node-slim = pkgs.nodejs-slim_24;
-    in rec {
-      devShells = {
-        default = pkgs.mkShell {
-          packages = with pkgs; [
-            # svelte
-            node
-
-            # lint
-            alejandra
-
-            # util
-            bumper
-          ];
-          shellHook = pkgs.shellhook.ref;
-        };
-
-        update = pkgs.mkShell {
-          packages = with pkgs; [
-            renovate
-
-            # npm i
-            node
+  outputs =
+    {
+      nixpkgs,
+      utils,
+      trev,
+      semgrep-rules,
+      ...
+    }:
+    utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [
+            trev.overlays.packages
+            trev.overlays.libs
           ];
         };
+        node = pkgs.nodejs_24;
+        node-slim = pkgs.nodejs-slim_24;
+      in
+      rec {
+        devShells = {
+          default = pkgs.mkShell {
+            packages = with pkgs; [
+              # svelte
+              node
 
-        vulnerable = pkgs.mkShell {
-          packages = with pkgs; [
-            # svelte
-            node
+              # util
+              bumper
 
-            # nix
-            flake-checker
+              # nix
+              nixfmt
+            ];
+            shellHook = pkgs.shellhook.ref;
+          };
 
-            # actions
-            octoscan
-          ];
+          update = pkgs.mkShell {
+            packages = with pkgs; [
+              renovate
+
+              # npm i
+              node
+            ];
+          };
+
+          vulnerable = pkgs.mkShell {
+            packages = with pkgs; [
+              # svelte
+              node
+
+              # nix
+              flake-checker
+
+              # actions
+              octoscan
+            ];
+          };
         };
-      };
 
-      checks = pkgs.lib.mkChecks {
-        svelte = {
-          src = packages.default;
-          deps = with pkgs; [
-            opengrep
+        checks = pkgs.lib.mkChecks {
+          svelte = {
+            src = packages.default;
+            deps = with pkgs; [
+              opengrep
+            ];
+            script = ''
+              npx prettier --check .
+              npx eslint .
+              npx svelte-kit sync && npx svelte-check
+              opengrep scan --quiet --error --use-git-ignore --config="${semgrep-rules}/typescript"
+              opengrep scan --quiet --error --use-git-ignore --config="${semgrep-rules}/javascript"
+            '';
+          };
+
+          nix = {
+            src = ./.;
+            deps = with pkgs; [
+              nixfmt
+            ];
+            script = ''
+              nixfmt -c flake.nix
+            '';
+          };
+
+          actions = {
+            src = ./.;
+            deps = with pkgs; [
+              action-validator
+              octoscan
+              renovate
+            ];
+            script = ''
+              # github
+              action-validator .github/**/*.yaml
+              octoscan scan .github
+              renovate-config-validator .github/renovate.json
+
+              # gitea
+              action-validator .gitea/**/*.yaml
+              octoscan scan .gitea
+              renovate-config-validator .gitea/renovate.json
+            '';
+          };
+        };
+
+        packages.default = pkgs.buildNpmPackage (finalAttrs: {
+          pname = "svelte-template";
+          version = "0.1.0";
+          src = builtins.path {
+            name = "root";
+            path = ./.;
+          };
+          nodejs = node;
+
+          npmDeps = pkgs.importNpmLock {
+            npmRoot = finalAttrs.src;
+          };
+
+          npmConfigHook = pkgs.importNpmLock.npmConfigHook;
+
+          nativeBuildInputs = with pkgs; [
+            makeWrapper
           ];
-          script = ''
-            npx prettier --check .
-            npx eslint .
-            npx svelte-kit sync && npx svelte-check
-            opengrep scan --quiet --error --use-git-ignore --config="${semgrep-rules}/typescript"
-            opengrep scan --quiet --error --use-git-ignore --config="${semgrep-rules}/javascript"
+
+          doCheck = false;
+
+          installPhase = ''
+            runHook preInstall
+
+             mkdir -p $out/{bin,lib/node_modules/svelte-template}
+             cp -r build node_modules package.json $out/lib/node_modules/svelte-template
+
+             makeWrapper "${pkgs.lib.getExe node-slim}" "$out/bin/svelte-template" \
+               --add-flags "$out/lib/node_modules/svelte-template/build/index.js"
+
+             runHook postInstall
           '';
-        };
 
-        nix = {
-          src = ./.;
-          deps = with pkgs; [
-            alejandra
-          ];
-          script = ''
-            alejandra -q -c flake.nix
-          '';
-        };
+          meta = {
+            description = "svelte template";
+            mainProgram = "svelte-template";
+            homepage = "https://github.com/spotdemo4/svelte-template";
+            changelog = "https://github.com/spotdemo4/svelte-template/releases/tag/v${finalAttrs.version}";
+            license = pkgs.lib.licenses.mit;
+            platforms = pkgs.lib.platforms.all;
+          };
+        });
 
-        actions = {
-          src = ./.;
-          deps = with pkgs; [
-            action-validator
-            octoscan
-            renovate
-          ];
-          script = ''
-            # github
-            action-validator .github/**/*.yaml
-            octoscan scan .github
-            renovate-config-validator .github/renovate.json
-
-            # gitea
-            action-validator .gitea/**/*.yaml
-            octoscan scan .gitea
-            renovate-config-validator .gitea/renovate.json
-          '';
-        };
-      };
-
-      packages.default = pkgs.buildNpmPackage (finalAttrs: {
-        pname = "svelte-template";
-        version = "0.1.0";
-        src = builtins.path {
-          name = "root";
-          path = ./.;
-        };
-        nodejs = node;
-
-        npmDeps = pkgs.importNpmLock {
-          npmRoot = finalAttrs.src;
-        };
-
-        npmConfigHook = pkgs.importNpmLock.npmConfigHook;
-
-        nativeBuildInputs = with pkgs; [
-          makeWrapper
-        ];
-
-        doCheck = false;
-
-        installPhase = ''
-          runHook preInstall
-
-           mkdir -p $out/{bin,lib/node_modules/svelte-template}
-           cp -r build node_modules package.json $out/lib/node_modules/svelte-template
-
-           makeWrapper "${pkgs.lib.getExe node-slim}" "$out/bin/svelte-template" \
-             --add-flags "$out/lib/node_modules/svelte-template/build/index.js"
-
-           runHook postInstall
-        '';
-
-        meta = {
-          description = "svelte template";
-          mainProgram = "svelte-template";
-          homepage = "https://github.com/spotdemo4/svelte-template";
-          changelog = "https://github.com/spotdemo4/svelte-template/releases/tag/v${finalAttrs.version}";
-          license = pkgs.lib.licenses.mit;
-          platforms = pkgs.lib.platforms.all;
-        };
-      });
-
-      formatter = pkgs.alejandra;
-    });
+        formatter = pkgs.nixfmt;
+      }
+    );
 }
