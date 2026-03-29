1c90ce4f42485a6e350e22224ea4c53afe25ec8d
trev <me@trev.xyz>
2026-03-29 05:55:03 -0400

    chore: use new syntax

--- flake.nix
+++ flake.nix
@@ -22,32 +22,18 @@
 
   outputs =
     {
-      nixpkgs,
+      self,
       trev,
       ...
     }:
     trev.libs.mkFlake (
-      system:
-      let
-        pkgs = import nixpkgs {
-          inherit system;
-          overlays = [
-            trev.overlays.packages
-            trev.overlays.libs
-          ];
-        };
-        fs = pkgs.lib.fileset;
-        node = pkgs.nodejs_24;
-        node-slim = pkgs.nodejs-slim_24;
-      in
-      rec {
+      system: pkgs: {
         devShells = {
           default = pkgs.mkShell {
-            name = "dev";
             shellHook = pkgs.shellhook.ref;
             packages = with pkgs; [
               # svelte
-              node
+              nodejs_24
 
               # formatters
               nixfmt
@@ -63,47 +49,36 @@
           };
 
           bump = pkgs.mkShell {
-            name = "bump";
             packages = with pkgs; [
               bumper
             ];
           };
 
           release = pkgs.mkShell {
-            name = "release";
             packages = with pkgs; [
               flake-release
             ];
           };
 
           update = pkgs.mkShell {
-            name = "update";
             packages = with pkgs; [
               renovate
-
-              # npm i
-              node
+              node # npm i
             ];
           };
 
           vulnerable = pkgs.mkShell {
-            name = "vulnerable";
             packages = with pkgs; [
-              # svelte
-              node
-
-              # nix
-              flake-checker
-
-              # actions
-              octoscan
+              node # svelte
+              flake-checker # nix
+              octoscan # actions
             ];
           };
         };
 
-        checks = pkgs.lib.mkChecks {
+        checks = pkgs.mkChecks {
           svelte = {
-            src = packages.default;
+            src = self.packages.${system}.default;
             script = ''
               npx prettier --check .
               npx eslint .
@@ -111,24 +86,22 @@
             '';
           };
 
-          nix = {
-            src = fs.toSource {
-              root = ./.;
-              fileset = fs.fileFilter (file: file.hasExt "nix") ./.;
-            };
+          actions = {
+            root = ./.;
+            fileset = ./.github/workflows;
             deps = with pkgs; [
-              nixfmt-tree
+              action-validator
+              octoscan
             ];
-            script = ''
-              treefmt --ci
+            forEach = ''
+              action-validator "$file"
+              octoscan scan "$file"
             '';
           };
 
           renovate = {
-            src = fs.toSource {
-              root = ./.github;
-              fileset = ./.github/renovate.json;
-            };
+            root = ./.github;
+            fileset = ./.github/renovate.json;
             deps = with pkgs; [
               renovate
             ];
@@ -137,117 +110,74 @@
             '';
           };
 
-          actions = {
-            src = fs.toSource {
-              root = ./.github/workflows;
-              fileset = ./.github/workflows;
-            };
+          nix = {
+            root = ./.;
+            filter = file: file.hasExt "nix";
             deps = with pkgs; [
-              action-validator
-              octoscan
+              nixfmt
             ];
-            script = ''
-              action-validator **/*.yaml
-              octoscan scan .
+            forEach = ''
+              nixfmt --check "$file"
             '';
           };
         };
 
-        apps = pkgs.lib.mkApps {
-          dev.script = "npm run dev";
+        apps = pkgs.mkApps {
+          dev = "npm run dev";
         };
 
-        packages = with pkgs.lib; rec {
-          default = pkgs.buildNpmPackage (finalAttrs: {
-            pname = "svelte-template";
-            version = "0.6.1";
+        packages.default = pkgs.buildNpmPackage (finalAttrs: {
+          pname = "svelte-template";
+          version = "0.6.1";
+          nodejs = pkgs.nodejs_24;
 
-            src = fs.toSource {
-              root = ./.;
-              fileset = fs.difference ./. (
-                fs.unions [
-                  ./.github
-                  ./.vscode
-                  ./flake.nix
-                  ./flake.lock
-                ]
-              );
-            };
-
-            nodejs = node;
-            npmConfigHook = pkgs.importNpmLock.npmConfigHook;
-            npmDeps = pkgs.importNpmLock {
-              npmRoot = finalAttrs.src;
-            };
-
-            nativeBuildInputs = with pkgs; [
-              makeWrapper
-            ];
-
-            installPhase = ''
-              runHook preInstall
-
-               mkdir -p $out/{bin,lib/node_modules/svelte-template}
-               cp -r build node_modules package.json $out/lib/node_modules/svelte-template
-
-               makeWrapper "${getExe node-slim}" "$out/bin/svelte-template" \
-                 --add-flags "$out/lib/node_modules/svelte-template/build/index.js"
-
-               runHook postInstall
-            '';
-
-            meta = {
-              description = "svelte template";
-              mainProgram = "svelte-template";
-              homepage = "https://github.com/spotdemo4/svelte-template";
-              changelog = "https://github.com/spotdemo4/svelte-template/releases/tag/v${finalAttrs.version}";
-              license = licenses.mit;
-              platforms = platforms.all;
-            };
-          });
-
-          image = pkgs.dockerTools.buildLayeredImage {
-            name = default.pname;
-            tag = default.version;
-
-            contents = with pkgs; [
-              dockerTools.caCertificates
-            ];
-
-            created = "now";
-            meta = default.meta;
-
-            config = {
-              Entrypoint = [ "${meta.getExe default}" ];
-              Labels = {
-                "org.opencontainers.image.title" = default.pname;
-                "org.opencontainers.image.description" = default.meta.description;
-                "org.opencontainers.image.version" = default.version;
-                "org.opencontainers.image.source" = default.meta.homepage;
-                "org.opencontainers.image.licenses" = default.meta.license.spdxId;
-              };
-            };
+          src = pkgs.lib.fileset.toSource {
+            root = ./.;
+            fileset = pkgs.lib.fileset.difference ./. (
+              pkgs.lib.fileset.unions [
+                ./.github
+                ./.vscode
+                ./flake.nix
+                ./flake.lock
+              ]
+            );
           };
+          npmDeps = pkgs.importNpmLock {
+            npmRoot = finalAttrs.src;
+          };
+          npmConfigHook = pkgs.importNpmLock.npmConfigHook;
 
-          # cross-compilation
-          linux-amd64 = deno.compile {
-            package = default;
-            target = "x86_64-unknown-linux-gnu";
+          nativeBuildInputs = with pkgs; [
+            makeWrapper
+          ];
+
+          installPhase = ''
+            runHook preInstall
+
+             mkdir -p $out/{bin,lib/node_modules/svelte-template}
+             cp -r build node_modules package.json $out/lib/node_modules/svelte-template
+
+             makeWrapper "${pkgs.lib.getExe pkgs.nodejs-slim_24}" "$out/bin/svelte-template" \
+               --add-flags "$out/lib/node_modules/svelte-template/build/index.js"
+
+             runHook postInstall
+          '';
+
+          meta = {
+            description = "svelte template";
+            mainProgram = "svelte-template";
+            license = pkgs.lib.licenses.mit;
+            platforms = pkgs.lib.platforms.all;
+            homepage = "https://github.com/spotdemo4/svelte-template";
+            changelog = "https://github.com/spotdemo4/svelte-template/releases/tag/v${finalAttrs.version}";
           };
-          linux-arm64 = deno.compile {
-            package = default;
-            target = "aarch64-unknown-linux-gnu";
-          };
-          darwin-arm64 = deno.compile {
-            package = default;
-            target = "aarch64-apple-darwin";
-          };
-          windows-amd64 = deno.compile {
-            package = default;
-            target = "x86_64-pc-windows-msvc";
-          };
+        });
+
+        images.default = pkgs.mkImage self.packages.${system}.default {
+          contents = with pkgs; [ dockerTools.caCertificates ];
         };
 
+        schemas = trev.schemas;
         formatter = pkgs.nixfmt-tree;
       }
     );
